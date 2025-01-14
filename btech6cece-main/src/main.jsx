53673191a406bc1145df25d0b630c3a07972105c
Ankit <iankitraj18@gmail.com>
2025-01-14 21:43:21 +0530

    add

---
+++ btech6cece-main/src/main.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client";
+import TableDisp from "./lec0601/exercise1";
+
+createRoot(document.getElementById("root")).render(
+  <div>
+    <TableDisp />
+  </div>
+);
